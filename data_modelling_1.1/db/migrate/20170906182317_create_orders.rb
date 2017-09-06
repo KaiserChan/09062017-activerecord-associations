c2581084d90dbe6a9cec990ef790a29b8de486ef
KaiserChan <chankaiserkk@gmail.com>
2017-09-06 14:38:00 -0400

    updated data_modelling 1.1 ... completed modelling and migration

--- data_modelling_1.1/db/migrate/20170906182317_create_orders.rb
+++ data_modelling_1.1/db/migrate/20170906182317_create_orders.rb
@@ -1,9 +1,10 @@
-class CreateOrder < ActiveRecord::Migration[5.1]
+class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.integer :order_number
-      t.integer :date
+      t.date :date
       t.integer :customer_id
+      t.timestamps
     end
   end
 end
--- data_modelling_1.1/db/schema.rb
+++ data_modelling_1.1/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906170714) do
+ActiveRecord::Schema.define(version: 20170906182317) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20170906170714) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "order_number"
-    t.integer "date"
+    t.date "date"
     t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
