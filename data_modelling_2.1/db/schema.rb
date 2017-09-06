dfb54e52c52d36d582613007dd1e127cddfa7485
KaiserChan <chankaiserkk@gmail.com>
2017-09-06 17:33:16 -0400

    updated data_modelling 2.1 - created viewer model and migration

--- data_modelling_2.1/db/schema.rb
+++ data_modelling_2.1/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906212558) do
+ActiveRecord::Schema.define(version: 20170906212901) do
 
   create_table "films", force: :cascade do |t|
     t.string "title"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "viewers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
