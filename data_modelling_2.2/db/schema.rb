f9caffd862a67d96fe936e470f267c1cb60a23bf
KaiserChan <chankaiserkk@gmail.com>
2017-09-06 22:48:56 -0400

    updated data_modelling 2.2 - created readers model and migration

--- data_modelling_2.2/db/schema.rb
+++ data_modelling_2.2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907023318) do
+ActiveRecord::Schema.define(version: 20170907023944) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170907023318) do
   create_table "chapters", force: :cascade do |t|
     t.string "title"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "readers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
